eb0921e57fa800362fb7386840f87460d0bf28d6
sithlord48 <sithlord48@gmail.com>
2024-10-22 14:49:19 -0400

    deskflow: renamed from synergy-core

--- Formula/d/deskflow.rb
+++ Formula/d/deskflow.rb
@@ -1,25 +1,24 @@
-class SynergyCore < Formula
-  desc "Synergy, the keyboard and mouse sharing tool"
-  homepage "https://symless.com/synergy"
-  url "https://github.com/symless/synergy/archive/refs/tags/1.15.1+r1.tar.gz"
-  version "1.15.1"
-  sha256 "42fbf26c634d2947c7efc45da8c9a153387bcdcb19c1102a4f7c4e95aad5c708"
+class Deskflow < Formula
+  desc "Network keyboard and mouse sharing tool"
+  homepage "https://deskflow.org"
+  url "https://github.com/deskflow/deskflow/archive/refs/tags/v1.17.0.tar.gz"
+  sha256 "5ba7fd70ca13f909ef23d6a5db250ebfae1e1f6c2ac89daf8d4cb3adc9e53538"
 
-  # The synergy-core/LICENSE file contains the following preamble:
+  # The deskflow/LICENSE file contains the following preamble:
   #   This program is released under the GPL with the additional exemption
   #   that compiling, linking, and/or using OpenSSL is allowed.
   # This preamble is followed by the text of the GPL-2.0.
   #
-  # The synergy-core license is a free software license but it cannot be
+  # The deskflow license is a free software license but it cannot be
   # represented with the brew `license` statement.
   #
   # The GitHub Licenses API incorrectly says that this project is licensed
   # strictly under GPLv2 (rather than GPLv2 plus a special exception).
-  # This requires synergy-core to appear as an exception in:
+  # This requires deskflow to appear as an exception in:
   #   audit_exceptions/permitted_formula_license_mismatches.json
   # That exception can be removed if the nonfree GitHub Licenses API is fixed.
   license :cannot_represent
-  head "https://github.com/symless/synergy-core.git", branch: "master"
+  head "https://github.com/deskflow/deskflow.git", branch: "master"
 
   # This repository contains old 2.0.0 tags, one of which uses a stable tag
   # format (`v2.0.0-stable`), despite being marked as "pre-release" on GitHub.
@@ -27,7 +26,7 @@ class SynergyCore < Formula
   # to worry about missing a new 2.0.0 version in the future.
   livecheck do
     url :stable
-    regex(/[^"' >]*?v?(\d+(?:\.\d+)+)[^"' >]*?/i)
+    regex(/^v\d+.\d+.\d+/i)
     strategy :github_latest
   end
 
@@ -42,26 +41,15 @@ class SynergyCore < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "cli11"
+  depends_on :macos
   depends_on "openssl@3"
   depends_on "pugixml"
   depends_on "qt"
+  depends_on "tomlplusplus"
 
   on_macos do
     depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1402
-  end
-
-  on_linux do
-    depends_on "pkg-config" => :build
-    depends_on "gdk-pixbuf"
-    depends_on "glib"
-    depends_on "libnotify"
-    depends_on "libx11"
-    depends_on "libxext"
-    depends_on "libxi"
-    depends_on "libxinerama"
-    depends_on "libxkbfile"
-    depends_on "libxrandr"
-    depends_on "libxtst"
   end
 
   fails_with :clang do
@@ -70,7 +58,7 @@ class SynergyCore < Formula
   end
 
   fails_with gcc: "5" do
-    cause "synergy-core requires C++17 support"
+    cause "deskflow requires C++17 support"
   end
 
   def install
@@ -86,22 +74,21 @@ class SynergyCore < Formula
     end
 
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DBUILD_TESTS:BOOL=OFF", "-DCMAKE_INSTALL_DO_STRIP=1",
-                    "-DSYSTEM_PUGIXML:BOOL=ON"
+                    "-DBUILD_TESTS:BOOL=OFF", "-DCMAKE_INSTALL_DO_STRIP=1"
 
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
     if OS.mac?
       prefix.install buildpath/"build/bundle"
-      bin.install_symlink prefix/"bundle/Synergy.app/Contents/MacOS/synergy"  # main GUI program
-      bin.install_symlink prefix/"bundle/Synergy.app/Contents/MacOS/synergys" # server
-      bin.install_symlink prefix/"bundle/Synergy.app/Contents/MacOS/synergyc" # client
+      bin.install_symlink prefix/"bundle/Deskflow.app/Contents/MacOS/deskflow" # main GUI program
+      bin.install_symlink prefix/"bundle/Deskflow.app/Contents/MacOS/deskflow-server" # server
+      bin.install_symlink prefix/"bundle/Deskflow.app/Contents/MacOS/deskflow-client" # client
     end
   end
 
   service do
-    run [opt_bin/"synergy"]
+    run [opt_bin/"deskflow"]
     run_type :immediate
   end
 
@@ -109,22 +96,22 @@ class SynergyCore < Formula
     # Because we used `license :cannot_represent` above, tell the user how to
     # read the license.
     s = <<~EOS
-      Read the synergy-core license here:
+      Read the deskflow license here:
         #{opt_prefix}/LICENSE
     EOS
     # The binaries built by brew are not signed by a trusted certificate, so the
     # user may need to revoke all permissions for 'Accessibility' and re-grant
-    # them when upgrading synergy-core.
+    # them when upgrading deskflow.
     on_macos do
       s += "\n" + <<~EOS
-        Synergy requires the 'Accessibility' permission.
+        Deskflow requires the 'Accessibility' permission.
         You can grant this permission by navigating to:
           System Preferences -> Security & Privacy -> Privacy -> Accessibility
 
-        If Synergy still doesn't work, try clearing the 'Accessibility' list:
+        If Deskflow still doesn't work, try clearing the 'Accessibility' list:
           sudo tccutil reset Accessibility
         You can then grant the 'Accessibility' permission again.
-        You may need to clear this list each time you upgrade synergy-core.
+        You may need to clear this list each time you upgrade deskflow.
       EOS
       # On ARM, macOS is even more picky when dealing with applications not signed
       # by a trusted certificate, and, for whatever reason, both the app bundle and
@@ -140,8 +127,8 @@ class SynergyCore < Formula
         s += "\n" + <<~EOS
           On ARM macOS machines, the 'Accessibility' permission must be granted to
           both of the following two items:
-            (1) #{opt_prefix}/bundle/Synergy.app
-            (2) #{opt_bin}/synergy
+            (1) #{opt_prefix}/bundle/Deskflow.app
+            (2) #{opt_bin}/deskflow
         EOS
       end
     end
@@ -149,15 +136,10 @@ class SynergyCore < Formula
   end
 
   test do
-    assert_match(/synergys: no configuration available\n$/,
-                 shell_output("#{opt_bin}/synergys 2>&1", 4))
-    assert_match(/synergyc: a server address or name is required$/,
-                 shell_output("#{opt_bin}/synergyc 2>&1", 3).split("\n")[0])
-
     version_string = Regexp.quote(version.major_minor_patch)
-    assert_match(/synergys v#{version_string}[-.0-9a-z]*, protocol v/,
-                 shell_output("#{opt_bin}/synergys --version").lines.first)
-    assert_match(/synergyc v#{version_string}[-.0-9a-z]*, protocol v/,
-                 shell_output("#{opt_bin}/synergyc --version").lines.first)
+    assert_match(/deskflow-server v#{version_string}[-.0-9a-z]*, protocol v/,
+                 shell_output("#{opt_bin}/deskflow-server --version").lines.first)
+    assert_match(/deskflow-client v#{version_string}[-.0-9a-z]*, protocol v/,
+                 shell_output("#{opt_bin}/deskflow-client --version").lines.first)
   end
 end
